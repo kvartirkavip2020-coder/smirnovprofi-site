8973357187253dc7da991c63ab44692f45a612e1
kvartirkavip2020-coder <kvartirka.vip2020@gmail.com>
2026-06-15 06:17:36 +0500

    Update import paths for components in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,4 @@
-import Hero from "@/components/Hero";
-import ServicesGrid from "@/components/ServicesGrid";
-import OneWindowGrid from "@/components/OneWindowGrid";
-import ContactForm from "@/components/ContactForm";
-
-export default function Home() {
-  return (
-    <>
-      <Hero />
-      <ServicesGrid />
-      <OneWindowGrid />
-      <ContactForm />
-    </>
-  );
-}
+import Hero from "../components/Hero";
+import ServicesGrid from "../components/ServicesGrid";
+import OneWindowGrid from "../components/OneWindowGrid";
+import ContactForm from "../components/ContactForm";
