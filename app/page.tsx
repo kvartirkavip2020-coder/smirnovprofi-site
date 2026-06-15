4a16c4fe94f5ecb5d145438869722f4c49381070
kvartirkavip2020-coder <kvartirka.vip2020@gmail.com>
2026-06-15 23:22:45 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,19 @@ import Hero from "../components/Hero";
 import TrustBlock from "../components/TrustBlock";
 import ServicesGrid from "../components/ServicesGrid";
 import OneWindowGrid from "../components/OneWindowGrid";
+import About from "../components/About";
 import ContactForm from "../components/ContactForm";
 
 export default function Page() {
   return (
     <main>
       <Hero />
-
+      <TrustBlock />
       <ServicesGrid />
-
       <OneWindowGrid />
+      <About />
 
-      <section style={{ padding: "80px 20px", textAlign: "center" }}>
-        <h2>Оставить заявку</h2>
+      <section id="contacts">
         <ContactForm />
       </section>
     </main>
